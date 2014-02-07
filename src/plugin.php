370d9d5fc4c579c3c10af19b06cf5f4cd744af42
Foxaii <nick@foxaii.com>
2014-02-07 19:55:12 +0000

    Fix for SSL

--- src/plugin.php
+++ src/plugin.php
@@ -10,12 +10,13 @@ class Roots_Rewrites {
     register_deactivation_hook($path, array($this, 'flush_rewrites'));
 
     $get_theme_name = explode('/themes/', get_template_directory());
+    $home_url = array(home_url('/', 'http'), home_url('/', 'https'));
 
     /**
      * Define helper constants
      */
-    define('RELATIVE_PLUGIN_PATH',  str_replace(home_url() . '/', '', plugins_url()));
-    define('RELATIVE_CONTENT_PATH', str_replace(home_url() . '/', '', content_url()));
+    define('RELATIVE_PLUGIN_PATH',  str_replace($home_url, '', plugins_url()));
+    define('RELATIVE_CONTENT_PATH', str_replace($home_url, '', content_url()));
     define('THEME_NAME',            next($get_theme_name));
     define('THEME_PATH',            RELATIVE_CONTENT_PATH . '/themes/' . THEME_NAME);
 
