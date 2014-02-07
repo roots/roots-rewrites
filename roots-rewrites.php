e54c3ccdb1a75f3b488bf6090a9e68bef29ee355
Foxaii <nick@foxaii.com>
2014-02-07 16:40:13 +0000

    Rework, Auto Flush Rewrites

--- roots-rewrites.php
+++ roots-rewrites.php
@@ -10,6 +10,20 @@ License:            MIT License
 License URI:        http://opensource.org/licenses/MIT
 */
 
+/**
+ * Rewrites do not happen for multisite installations or child themes
+ *
+ * Rewrite:
+ *   /wp-content/themes/themename/assets/css/ to /assets/css/
+ *   /wp-content/themes/themename/assets/js/  to /assets/js/
+ *   /wp-content/themes/themename/assets/img/ to /assets/img/
+ *   /wp-content/plugins/                     to /plugins/
+ *
+ * If you aren't using Apache, Nginx configuration settings can be found in the README
+ */
+
 defined('ABSPATH') or die('A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools. - Douglas Adams');
 
 include_once('src/plugin.php');
+
+new Roots_Rewrites(__FILE__);
